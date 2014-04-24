0c087783ecbdf5ec6d8c8b3049a9c372c2223e1e
Hugo Colato. <hcolato@gmail.com>
2014-04-24 15:58:50 -0600

    Esta clase es utilizada para crear variables objeto de tipo Room.

---
+++ ejemplos/src/sv/edu/ufg/maestria/tje0/ejemplos/arreglos/Room.java
@@ -0,0 +1,30 @@
+package sv.edu.ufg.maestria.tje0.ejemplos.arreglos;
+
+public class Room {
+
+	  //instance variables
+	  String name;
+	  int width;
+	  int length;
+	  double costSY;  //cost per square yard in colones
+	  int area;  //width * height
+	  double total;  //area * costSY
+
+	  //instance methods
+	  
+	  void calc(){
+	    this.area = this.length * this.width;
+	    this.total = this.area * this.costSY;
+	  }
+
+	  void print(){
+	    System.out.println("*** Room: " + this.name + " ***");
+	    System.out.println("L=" + this.length);
+	    System.out.println("W=" + this.width);
+	    System.out.println("C=" + this.costSY);
+	    System.out.println("A=" + this.area);
+	    System.out.println("T=" + this.total);
+	    System.out.println(); //prints blank line
+	  }
+
+}
